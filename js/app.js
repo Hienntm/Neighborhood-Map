130c2e960bda862fa20a2c2213b80d71a3a98a14
HienNTM <hienntm134@gmail.com>
2016-09-07 16:59:41 +0700

    2nd commit
    + all features are done by Knockout (not jQuery)
    + also filter markers when clicking location name in the menu list

--- js/app.js
+++ js/app.js
@@ -1,47 +1,59 @@
 var map;
 var locations = [
-    { title: 'CENTRAL PARK ZOO', location: { lat: 40.767017, lng: -73.971966 } },
-    { title: 'THE METROPOLITAN MUSEUM OF ART', location: { lat: 40.779187, lng: -73.963535 } },
-    { title: 'ROCKEFELLER CENTER', location: { lat: 40.75861, lng: -73.978209 } },
-    { title: 'THE PRESS LOUNGE', location: { lat: 40.764543, lng: -73.995958 } },
-    { title: 'BROADWAY DANCE CENTER', location: { lat: 40.759447, lng: -73.989612 } },
-    { title: 'UNITARIAN CHURCH OF ALL SOULS', location: { lat: 40.775329, lng: -73.958195 } },
-    { title: 'GAONNURI RESTAURANT', location: { lat: 40.747742, lng: -73.987875 } },
-    { title: 'HALAL RESTAURANT', location: { lat: 40.793134, lng: -73.940832 } },
-    { title: 'HOT BREAD KITCHEN', location: { lat: 40.798315, lng: -73.943913 } },
-    { title: 'SILVANA', location: { lat: 40.804454, lng: -73.955898 } },
-    { title: 'NEW YORK PUBLIC LIBRARY', location: { lat: 40.802981, lng: -73.953536 } },
-    { title: 'AMERICAN MUSEUM OF NATURAL HISTORY', location: { lat: 40.781324, lng: -73.973988 } },
-    { title: 'BROADWAY THEATRE', location: { lat: 40.763365, lng: -73.98307 } },
+    { title: 'Central Park Zoo', location: { lat: 40.767017, lng: -73.971966 } },
+    { title: 'The Metropolitan Museum Of Art', location: { lat: 40.779187, lng: -73.963535 } },
+    { title: 'Rockefeller Center', location: { lat: 40.75861, lng: -73.978209 } },
+    { title: 'The Press Lounge', location: { lat: 40.764543, lng: -73.995958 } },
+    { title: 'Broadway Dance Center', location: { lat: 40.759447, lng: -73.989612 } },
+    { title: 'Unitarian Church Of All Souls', location: { lat: 40.775329, lng: -73.958195 } },
+    { title: 'Gaonnuri Restaurant', location: { lat: 40.747742, lng: -73.987875 } },
+    { title: 'Halal Restaurant', location: { lat: 40.793134, lng: -73.940832 } },
+    { title: 'Hot Bread Kitchen', location: { lat: 40.798315, lng: -73.943913 } },
+    { title: 'Silvana', location: { lat: 40.804454, lng: -73.955898 } },
+    { title: 'New York Public Library', location: { lat: 40.802981, lng: -73.953536 } },
+    { title: 'American Museum Of Natural History', location: { lat: 40.781324, lng: -73.973988 } },
+    { title: 'Broadway Theatre', location: { lat: 40.763365, lng: -73.98307 } },
 ];
-var markers = [];
 
 var locations_title = [];
-for (var i = 0; i < locations.length; i++) {
-    locations_title.push(locations[i].title);
-}
+locations.forEach(function(location){
+    locations_title.push(location.title);
+});
 
 //NEWYORK TIMES API    
 function loadData(position) {
 
-    var NYT_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + position + "&sort=newest&api-key=ce617c645d934270aa9e45a0a2e0df8d"
-
-    $.ajax({
+    var NYT_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + position + "&sort=newest&api-key=ce617c645d934270aa9e45a0a2e0df8d";
+    $.getJSON({
         url: NYT_url,
         method: 'GET',
     }).done(function (data) {
-        $(".nytHeader").text('New York Times Articles About This Place');
         articles = data.response.docs;
+        var items = "";
         for (i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $(".nytItem").append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p class="snippet">' + article.snippet + '</p>' + '</li>');
-        };
+            items += '<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p class="snippet">' + article.snippet + '</p>' + '</li>';
+        }
+        infowindow.setContent(
+            "<div class='infowindow'>" +
+            "<h2 class='title'>" + position + "</h2>" +
+            "<h3 class='nytHeader'>New York Times Articles About This Place<h3>" +
+            "<ul class='nytItem'>" + items + "</ul>" +
+            "</div>");      
     }).fail(function (e) {
-        $(".nytHeader").text('New York Times Articles could not be loaded');
-    });
-}
+        infowindow.setContent(
+            "<div class='infowindow'>" +
+            "<h2 class='title'>" + position + "</h2>" +
+            "<h3 class='nytHeader'>New York Times Articles could not be loaded<h3>" +
+            "</div>"); 
+    }); 
+    
+};
+
 
 //initialize the map
+var markers = [];
+var infowindow;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 40.784869, lng: -73.96967 },
@@ -49,9 +61,9 @@ function initMap() {
         mapTypeControl: false
     });
 
-    var infowindow = new google.maps.InfoWindow();
-    for (var i = 0; i < locations.length; i++) {
+    infowindow = new google.maps.InfoWindow();
 
+    for (var i = 0; i < locations.length; i++) {
         var position = locations[i].location;
         var title = locations[i].title;
 
@@ -60,56 +72,24 @@ function initMap() {
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
-            id: i
+            id: i,
+            icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",   
         });
 
         markers.push(marker);
-
+        
         //Display infowindow when clicking on markers
         marker.addListener('click', function () {
-            infowindow.setContent(
-                "<div class='infowindow'>" +
-                "<h2 class='title'>" + this.title + "</h2>" +
-                "<h3 class='nytHeader'><h3>" +
-                "<ul class='nytItem'></ul>" +
-                "</div>"
-            );
+            this.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
             loadData(this.title);
-            infowindow.open(map, this);
+            infowindow.open(map, this);          
         });
-
     }
 
-    //Display infowindow when clicking on list items
-    $(".location").on("click", function () {
-        var currentlocation = this.innerHTML
-        var index = locations_title.indexOf(currentlocation);
-        infowindow.setContent(
-            "<div class='infowindow'>" +
-            "<h2 class='title'>" + currentlocation + "</h2>" +
-            "<h3 class='nytHeader'><h3>" +
-            "<ul class='nytItem'></ul>" +
-            "</div>"
-        );
-        loadData(currentlocation);
-        infowindow.open(map, markers[index]);
-
-        console.log(this);
+    infowindow.addListener('closeclick', function() {
+        markers.forEach(function(marker) {
+            marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+        });    
     });
-
 }
 
-//toggle menu when clicking the hamburger icon
-function toggleMenu() {
-    $("#list").toggleClass("hidden");
-    
-}
-$("#menu").on("click", toggleMenu);
-
-//toggle button when clicking the filter/reset button
-function toggleButton() {
-    $("#filter").toggleClass("hidden");
-    $("#reset").toggleClass("hidden");
-}
-$("#filter").on("click", toggleButton);
-$("#reset").on("click", toggleButton);
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,24 +1,58 @@
 function ViewModel() {
-    this.locations = ko.observableArray(locations);
-    
-    this.searchInput = ko.observable();
-    
-    //filter locations]
-    this.filterLocations = function() {
-           
-        var keyword = this.searchInput().toUpperCase();
+    var self = this;
 
-        $(".location:not(:contains("+ keyword + "))").css("display", "none");
+    self.locations = ko.observableArray(locations);
+    self.markers = ko.observableArray(markers);
+    self.searchInput = ko.observable();
+   
+    //Display infowindow when clicking on list items
+    self.loadInfoWindow = function() {
+        var currentLocation = this.title;
+        var index = locations_title.indexOf(currentLocation);
+        loadData(currentLocation);
+        markers[index].setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+        infowindow.open(map, markers[index]); 
+    };
+
+    // ( ☰ ICON ) toggle menu when clicking the hamburger icon
+    self.menuStatus = ko.observable(0);
+    self.toggleMenu = function() {
+        var current = self.menuStatus();
+        self.menuStatus(current+1);
+    };
+    
+    // (Filter BUTTON) filter locations list 
+    self.filterLocations = function() {
+        //captialize keyword         
+        var keyword = self.searchInput().toUpperCase();
+        var filterResult = self.locations().filter(function(location){
+            //captialize location name and find index of keyword within
+            var containKeyword = location.title.toUpperCase().indexOf(keyword);
+            return containKeyword !== -1;  
+        });
+        self.locations(filterResult);
+
+        console.log(filterResult);
+        filterResult.forEach(function(location, index){
+            filterResult[index] = location.title;
+        })
         
-        if(this.searchInput() === "") {
-            $(".location").css("display", "block");
-        }
-    }
+        //filter markers on map
+        markers.forEach(function(marker){
+            if (filterResult.indexOf(marker.title) === -1)
+                marker.setVisible(false);
+        });
+    };
 
-    this.resetLocations = function() {
-         $(".location").css("display", "block");
-    }
-    
+    // (Reset BUTTON) reset locations list
+    self.resetLocations = function() {
+        self.locations(locations);
+    };
 }
 
-ko.applyBindings(new ViewModel());
+var vimo = new ViewModel();
+
+ko.applyBindings(vimo);
+
+
+
